f1e0fb86ddf91b7063c21b89005f5bddba217bfb
perevalovds <perevalovds@gmail.com>
2019-12-09 21:10:22 +0500

    oF

--- mobilenet/utils.h
+++
@@ -1,8 +0,0 @@
-#ifndef UTILS_H
-#define UTILS_H
-
-float Accuracy_1(float *pfPred, int *pnLab, int nclass);
-float Accuracy_all(float *pfPred, int *pnLab, int nclass);
-void test(const char *pcTestListName, const char *pcTestLabelsName, const char *pcTestImageSet);
-
-#endif
